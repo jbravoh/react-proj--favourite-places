adec0183ad085bd841b71e8c95d5331af6b7e408
Jay-Ann Bravo-Harriott <jbravoharriott@gmail.com>
2021-08-12 15:48:39 +0100

    Add interface and JSX.Element content

--- src/components/FavouritePlace.tsx
+++ src/components/FavouritePlace.tsx
@@ -1,3 +1,18 @@
-function FavouritePlace(): JSX.Element {}
+interface FavouritePlaceProps {
+  image: string;
+  place: string;
+  location: string;
+  description: string;
+}
+
+function FavouritePlace(props: FavouritePlaceProps): JSX.Element {
+  return (
+    <section>
+      <h2>{props.place}</h2>
+      <h3>{props.location}</h3>
+      <p>{props.description}</p>
+    </section>
+  );
+}
 
 export default FavouritePlace;
