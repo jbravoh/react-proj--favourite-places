a6c0a106733ca319234b8f1effdb3f89f3264292
Jay-Ann Bravo-Harriott <jbravoharriott@gmail.com>
2021-08-13 14:13:39 +0100

    Add JSX.Elements

--- src/components/PageFooter.tsx
+++ src/components/PageFooter.tsx
@@ -1,3 +1,14 @@
-function PageFooter(): JSX.Element {}
+function PageFooter(): JSX.Element {
+  return (
+    <footer>
+      <p className="footer-title">Jay-Ann's Favourite Places</p>
+      <p className="credit">
+        {
+          "Photos by Robson Hatsukami Morgan, Luca Bravo, Eva Dang and Ryan Spencer on Unsplash"
+        }
+      </p>
+    </footer>
+  );
+}
 
 export default PageFooter;
