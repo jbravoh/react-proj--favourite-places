3fb39cc0a579f4a440e64a118c23869bcf52ac59
Jay-Ann Bravo-Harriott <jbravoharriott@gmail.com>
2021-08-13 18:56:29 +0100

    Update interface and add a key

--- src/components/FavouritePlace.tsx
+++ src/components/FavouritePlace.tsx
@@ -1,28 +1,39 @@
 import "../css/script.css";
 
 interface FavouritePlaceProps {
+  id: number;
   imgSrc: string;
   imgAlt: string;
   title: string;
-  name: string;
-  country: string;
+  location: {
+    city: string;
+    country: string;
+    mapURL: string;
+  };
   description: string;
-  mapURL: string;
 }
+// name: string;
+
+// country: string;
+
+// mapURL: string;
 
 function FavouritePlace(props: FavouritePlaceProps): JSX.Element {
   return (
-    <section className="place-container">
-      <img className="image" src={props.imgSrc} alt={props.imgAlt} />
-      <h2 className="place-title">{props.title}</h2>
-      <h3 className="place-location">
-        {props.name}, {props.country}
-      </h3>
-      <p className="place-description">{props.description}</p>
-      <p>
-        Here's {props.name} on <a href={props.mapURL}>Google Maps</a>.
-      </p>
-    </section>
+    <>
+      <section className="place-container" key={props.id}>
+        <img className="image" src={props.imgSrc} alt={props.imgAlt} />
+        <h2 className="place-title">{props.title}</h2>
+        <h3 className="place-location">
+          {props.location.city}, {props.location.country}
+        </h3>
+        <p className="place-description">{props.description}</p>
+        <p>
+          Here's {props.location.city} on{" "}
+          <a href={props.location.mapURL}>Google Maps</a>.
+        </p>
+      </section>
+    </>
   );
 }
 
