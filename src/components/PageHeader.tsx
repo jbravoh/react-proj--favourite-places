c973acbd40f15d221035f7781535317036b56d57
Jay-Ann Bravo-Harriott <jbravoharriott@gmail.com>
2021-08-13 14:13:54 +0100

    Add page title

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,3 +1,6 @@
-function PageHeader(): JSX.Element {}
+import "../css/script.css";
 
+function PageHeader(): JSX.Element {
+  return <h1 className="page-title">Jay-Ann's Favourite Places</h1>;
+}
 export default PageHeader;
