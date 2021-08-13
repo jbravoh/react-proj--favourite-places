c0dc10bfd185c18fc11ee525b332590346563a1a
Jay-Ann Bravo-Harriott <jbravoharriott@gmail.com>
2021-08-13 10:30:40 +0100

    Add images and import it to index.tsx file

---
+++ src/images/index.tsx
@@ -0,0 +1,6 @@
+import london from "./london.jpg";
+import miami from "./miami.jpg";
+import newOrleans from "./new-orleans.jpg";
+import newYork from "./new-york.jpg";
+
+export {london, miami, newOrleans, newYork};
